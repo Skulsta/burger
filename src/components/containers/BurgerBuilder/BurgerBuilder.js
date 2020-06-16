a01f15980b9dda0bc58192275d400a97852f69fb
skulsta <sti.sku@gmail.com>
2020-06-16 15:09:17 +0200

    Displaying a burger

--- src/components/containers/BurgerBuilder/BurgerBuilder.js
+++ src/components/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,11 @@
 import React, { Component, Fragment } from "react";
+import Burger from "../../Burger/Burger";
 
 class BurgerBuilder extends Component {
   render() {
     return (
       <Fragment>
-        <div>Burger</div>
-        <div>Build Controls</div>
+        <Burger />
       </Fragment>
     );
   }
